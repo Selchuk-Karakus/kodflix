f192ed09bd1c05dedf9e0d61410137da04166577
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-01-07 21:21:08 +0000

    add favourite movie cover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import winterSleepBanner from './winterSleep.logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Kodflix
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={winterSleepBanner} alt='Movie Banner' />
     </div>
   );
 }

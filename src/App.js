511aa09eb282cd8cf0eb9757c69ac1cc1b182ffc
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-01-09 16:15:12 +0000

    add the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -13,23 +13,41 @@ function App() {
         <div className='banner-row'>
             <div className='banner-column'>
               <img src={fargoBanner} alt='Movie Banner' />
+              <div class="overlay">
+                <div class="title"><h1>Fargo</h1></div>
+              </div>
             </div>
             <div className='banner-column'>
               <img src={theKnickBanner} alt='Movie Banner' />
+              <div class="overlay">
+                <div class="title"><h1>The Knick</h1></div>
+              </div>
             </div>
             <div className='banner-column'>
               <img src={vinlyBanner} alt='Movie Banner' />
+              <div class="overlay">
+                <div class="title"><h1>Vinly</h1></div>
+              </div>
             </div>
         </div>
         <div className='banner-row'>  
             <div className='banner-column'>
               <img src={theSopranosBanner} alt='Movie Banner' />
+              <div class="overlay">
+                <div class="title"><h1>The Sopranos</h1></div>
+              </div>
             </div>
             <div className='banner-column'>
               <img src={bizimkilerBanner} alt='Movie Banner' />
+              <div class="overlay">
+                <div class="title"><h1>Bizimkiler</h1></div>
+              </div>
             </div>
             <div className='banner-column'>
               <img src={howIMetYourMotherBanner} alt='Movie Banner' />
+              <div class="overlay">
+                <div class="title"><h1>How I Met Your Mother</h1></div>
+              </div>
             </div>
         </div>
     </div>

4fc5476b12c6ef2ac3c4860c36b9fb7daf6b8ad3
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-01-21 21:50:49 +0000

    introduce component state

--- src/components/TvShowList.js
+++ src/components/TvShowList.js
@@ -5,7 +5,7 @@ export default function TvShowList(props) {
   return ( 
     <Link to={props.id} className='item'>
         <img src={props.banner} alt={`${props.name} 'Movie Banner'`} />
-        <div class="overlay">
+        <div className="overlay">
           <h2>{props.name}</h2>
         </div>
     </Link>

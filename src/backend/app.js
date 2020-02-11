29ebb58ab05a9d82e51724d39e7ada0a61cbbbf7
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-11 14:58:48 +0000

    return a list of shows from the backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const tvShowList = require('./tvshowlist.js');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/rest/movies', (req, res) => {
-    res.send(tvShortList);
+    res.send(tvShowList);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

10ded64724bbe0b164d99c6273a9612b097ca40f
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-25 13:59:01 +0000

    add nodemon, improve setup of backend server

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const tvShowList = require('./tvshowlist.js');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/rest/movies', (req, res) => {
     res.send(tvShowList);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 

e7e47b9038e651838be187162678af80a3719142
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-02-26 15:12:47 +0000

    replace the TV show frontend data source, with the backend one

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express');
 const tvShowList = require('./tvshowlist.js');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+const path = require('path');
 
-app.get('/', (request, response) => response.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '../../build')));
 app.get('/rest/tvshowlist', (request, response) => {
     response.send(tvShowList);
 });
@@ -11,6 +12,10 @@ app.get('/rest/tvshowlist/:id', (request, response) => {
     
     response.send(tvShowList.find(item => item.id.toLowerCase() === request.params.id.toLowerCase()));
     
+});
+
+app.get('/', function(require, response) {
+    res.sendFile('index.html')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
